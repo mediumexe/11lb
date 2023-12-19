376ec3b5325b8a1ebcb67a3f7e86708850251782
mediumexe <149566514+mediumexe@users.noreply.github.com>
2023-12-19 16:16:57 +0300

    Add files via upload

---
+++ 3.pas
@@ -0,0 +1,11 @@
+﻿uses graphABC;
+var
+  i,k:integer;
+begin
+k:=0;
+for i:=1 to 9 do begin
+  setpencolor(clrandom);
+  circle(100,50+k,10);
+  k:=k+30;
+  end;
+end.
---
+++ 4.pas
@@ -0,0 +1,11 @@
+﻿uses graphABC;
+var 
+  i,k:integer;
+begin
+  k:=0;
+  for i:=1 to 10 do begin
+  circle(310,190,100-k);
+  k:=k+10;
+  end;
+ 
+end.
